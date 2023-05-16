6799422e9e8533c18e1669c386456af8839cf5d1
deansainer <determind2k@gmail.com>
2023-05-17 00:08:46 +0200

    commit

--- store/store_app/models.py
+++ store/store_app/models.py
@@ -16,7 +16,7 @@ class Product(models.Model):
     price = models.FloatField()
     image = models.ImageField(null=True, blank=True)
     color = models.CharField(max_length=25, null=True, blank=True)
-    made_available = models.IntegerField(null=True)
+    release_year = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
@@ -28,6 +28,16 @@ class Order(models.Model):
     complete = models.BooleanField(default=False, null=True)
     transaction_id = models.IntegerField(null=True, blank=True)
 
+    @property
+    def get_cart_total(self):
+        orderitems = self.orderitem_set.all()
+        cart_total = sum(item.get_item_total for item in orderitems)
+        return cart_total
+    @property
+    def get_cart_quantity(self):
+        orderitems = self.orderitem_set.all()
+        cart_quantity = sum([item.quantity for item in orderitems])
+
     def __str__(self):
         return str(self.id)
 
@@ -37,6 +47,11 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_item_total(self):
+        item_total = self.product.price * self.quantity
+        return item_total
 
 
 class ShippingAddress(models.Model):
--- store/store_app/views.py
+++ store/store_app/views.py
@@ -3,17 +3,40 @@ from .models import *
 
 
 def store(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_quantity': 0}
     products = Product.objects.all()
-    context = {'products': products}
+    context = {'products': products, 'items': items, 'order': order}
     return render(request, 'store_app/store.html', context)
 
 
 def cart(request):
-    return render(request, 'store_app/cart.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_quantity': 0}
+    context = {'items': items, 'order': order}
+    return render(request, 'store_app/cart.html', context)
 
 
 def checkout(request):
-    return render(request, 'store_app/checkout.html')
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total': 0, 'get_cart_quantity': 0}
+    context = {'items': items, 'order': order}
+    return render(request, 'store_app/checkout.html', context)
 
 
 def thanks(request):
