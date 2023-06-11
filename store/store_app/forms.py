253841edc17ad6ff58333d3afce2fd227597aad2
deansainer <determind2k@gmail.com>
2023-06-11 13:59:57 +0200

    updated gitignore file

--- store/store_app/forms.py
+++ store/store_app/forms.py
@@ -6,3 +6,11 @@ class ShippingAddressForm(forms.ModelForm):
     class Meta:
         model = ShippingAddress
         fields = '__all__'
+        widgets={
+            'customer': forms.Select(attrs={'class': 'form-control'} ),
+            'order': forms.Select(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'state': forms.TextInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            'zip_code': forms.TextInput(attrs={'class': 'form-control'}),
+        }
